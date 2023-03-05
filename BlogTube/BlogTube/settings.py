a20e66f79b141948acc1be984ad4b4b1f098d69d
Marck Lester <marck_lester@yahoo.com>
2023-03-05 13:19:19 +0000

    Register new app "posts"

--- BlogTube/BlogTube/settings.py
+++ BlogTube/BlogTube/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts.apps.PostsConfig' 
 ]
+
+# Marck added - 'posts.apps.PostsConfig' - to register the application in our Django project
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ BlogTube/posts/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    name = 'posts'
+
+
+# Marck - To set up configuration of our application here.
